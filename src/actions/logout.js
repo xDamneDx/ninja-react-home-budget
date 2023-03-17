b7f019926f5ebaf915d82664fabea8cc147d320a
xDamneDx <xdamnedx@gmail.com>
2023-03-17 11:32:33 +0200

    adding toast messages

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,12 +1,16 @@
 // React Router Dom Imports:
 import { redirect } from "react-router-dom";
 
+// Libraries:
+import { toast } from "react-toastify";
+
 // Helpers:
 import { deleteItem } from "../helpers";
 
 export async function logoutAction() {
   // delete the user
   deleteItem({ key: "userName" });
+  toast.success("You've deleted your account!");
 
   // return redirect
   return redirect("/");
