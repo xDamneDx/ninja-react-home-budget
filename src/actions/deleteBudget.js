f2741cd4d70d8cb23a5536e3c44ea2139d8aec3d
xDamneDx <xdamnedx@gmail.com>
2023-03-20 16:25:35 +0200

    adding buttons to cards

---
+++ src/actions/deleteBudget.js
@@ -0,0 +1,33 @@
+// React Router Dom Imports:
+import { redirect } from "react-router-dom";
+
+// Libraries:
+import { toast } from "react-toastify";
+
+// Helper Functions:
+import { deleteItem, getAllMatchingItems } from "../helpers";
+
+export function deleteBudget({ params }) {
+  try {
+    deleteItem({ key: "budgets", id: params.id });
+
+    const associatedExpenses = getAllMatchingItems({
+      category: "expenses",
+      key: "budgetId",
+      value: params.id,
+    });
+
+    associatedExpenses.forEach((expense) => {
+      deleteItem({
+        key: "expenses",
+        id: expense.id,
+      });
+    });
+
+    toast.success("Budget deleted successfully!");
+  } catch (error) {
+    throw new Error("There was a problem deleting your budget.");
+  }
+
+  return redirect("/");
+}
--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,3 +1,9 @@
+// React Router Dom Imports:
+import { Form, Link } from "react-router-dom";
+
+// Libraries:
+import { BanknotesIcon, TrashIcon } from "@heroicons/react/24/outline";
+
 // Helper Functions:
 import {
   calculateSpentByBudget,
@@ -5,7 +11,7 @@ import {
   formatPercentage,
 } from "../helpers";
 
-export default function BudgetItem({ budget }) {
+export default function BudgetItem({ budget, showDelete = false }) {
   const { id, name, amount, color } = budget;
   const spent = calculateSpentByBudget(id);
 
@@ -22,6 +28,35 @@ export default function BudgetItem({ budget }) {
         <small>{formatCurrency(spent)} spent</small>
         <small>{formatCurrency(amount - spent)} remaining</small>
       </div>
+      {showDelete ? (
+        <div className="flex-sm">
+          <Form
+            method="post"
+            action="delete"
+            onSubmit={(e) => {
+              if (
+                !confirm(
+                  "Are you sure you want to permanently delete this budget?"
+                )
+              ) {
+                e.preventDefault();
+              }
+            }}
+          >
+            <button type="submit" className="btn">
+              <span>Delete Budget</span>
+              <TrashIcon width={20} />
+            </button>
+          </Form>
+        </div>
+      ) : (
+        <div className="flex-sm">
+          <Link to={`/budget/${id}`} className="btn">
+            <span>View Details</span>
+            <BanknotesIcon width={20} />
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
